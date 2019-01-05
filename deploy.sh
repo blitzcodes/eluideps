abd8011ac60aae1f32b2d62d3a30d8416e152d0d
Sean 'Blitz' Homer <homer@therefore.ca>
2019-01-05 11:59:12 -0500

    Refreshing Code Base

---
+++ deploy.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+git init
+git add -A
+git commit -m 'Refreshing Code Base'
+
+git push -f git@github.com:blitzcodes/eluideps.git master:master
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:blitzcodes/eluideps.git master:gh-pages
+
+cd -
